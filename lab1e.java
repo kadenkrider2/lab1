782eb52f84b05c4008754064f82666c178d9492f
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-02-13 19:04:42 -0500

    Create lab1e.java

---
+++ lab1e.java
@@ -0,0 +1,11 @@
+public class ASCIIArt {
+	public static void main(String[] args) {
+		System.out.println(" _ _ _");
+		System.out.println("{o}{o}{o}");
+		System.out.println(" | | |");
+		System.out.println("\\|/\\|/\\|/");
+		System.out.println("[~~~~~~~~~]");
+		System.out.println(" |~~~~~~~|");
+		System.out.println(" |_______|");
+	}
+}
