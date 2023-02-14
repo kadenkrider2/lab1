45c29c52cf51d5a64d4c52dc54082b27fe1bc31c
kadenkrider2 <118944863+kadenkrider2@users.noreply.github.com>
2023-02-13 19:03:54 -0500

    Create lab1d.java

---
+++ lab1d.java
@@ -0,0 +1,9 @@
+public class Lab1d {
+    public static void main(String[] args) {
+        System.out.println("Lab1d:");
+        System.out.println("********");
+        System.out.println("  ********");
+        System.out.println("    ********");
+        System.out.println("      ********");
+    }
+}
